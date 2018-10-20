c7dc8f764bd04c19bab29fcfe4cd3eaf607f4297
TohruTakahashi <th.tk.2012@gmail.com>
2018-10-20 16:25:51 +0900

    インデントを調整。ActionTypesを使うように修正。

--- ReactFluxSample/ReactFluxSample/ClientApp/src/store/WeatherForecastsStore.js
+++ ReactFluxSample/ReactFluxSample/ClientApp/src/store/WeatherForecastsStore.js
@@ -1,37 +1,36 @@
 ﻿import { ReduceStore } from 'flux/utils';
 import Dispatcher from '../dispatcher/Dispatcher';
+import { ActionTypes } from '../constants/AppConstants';
 
-const requestWeatherForecastsType = 'REQUEST_WEATHER_FORECASTS';
-const receiveWeatherForecastsType = 'RECEIVE_WEATHER_FORECASTS';
 const initialState = { forecasts: [], isLoading: false };
 
 class WeatherForecastsStore extends ReduceStore {
-    getInitialState() {
-        return initialState;
+  getInitialState() {
+    return initialState;
+  }
+
+  reduce(state, action) {
+    state = state || initialState;
+
+    if (action.type === ActionTypes.requestWeatherForecastsType) {
+      return {
+        ...state,
+        startDateIndex: action.startDateIndex,
+        isLoading: true
+      };
     }
 
-    reduce(state, action) {
-        state = state || initialState;
-
-        if (action.type === requestWeatherForecastsType) {
-            return {
-                ...state,
-                startDateIndex: action.startDateIndex,
-                isLoading: true
-            };
-        }
-
-        if (action.type === receiveWeatherForecastsType) {
-            return {
-                ...state,
-                startDateIndex: action.startDateIndex,
-                forecasts: action.forecasts,
-                isLoading: false
-            };
-        }
-
-        return state;
+    if (action.type === ActionTypes.receiveWeatherForecastsType) {
+      return {
+        ...state,
+        startDateIndex: action.startDateIndex,
+        forecasts: action.forecasts,
+        isLoading: false
+      };
     }
+
+    return state;
+  }
 }
 
 const instance = new WeatherForecastsStore(Dispatcher);
